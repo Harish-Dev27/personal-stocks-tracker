75458114e0840339700d1968d5251fabf5f0d70f
Harish <shanharish.harish@gmail.com>
2026-07-25 12:13:32 +0530

    Added dataclass for parsing yfinance objects

--- src/interface/StocksInfo.py
+++ src/interface/StocksInfo.py
@@ -1,5 +1,7 @@
 import yfinance as yf
+import pandas as pd
 from utils.Logger import logger
+from model import StockQuote
 
 class StocksInfo:
 
@@ -16,9 +18,27 @@ class StocksInfo:
             stock_info_list = []
             for stock in self.stocks: 
                 stock_info = yf.Ticker(f"{stock.upper()}.NS")
-                stock_info_list.append({stock : stock_info.history(period="1d")})
+                stock_info_list.append(self.to_stock_quote(stock_info.history(period="1d")))
 
             return stock_info_list
         except Exception as e:
             logger.error(f"ERROR", "Unable to get stocks information. Reason: {e}")     
             return None
+
+    """
+    Mapper function to convert the DataFrame type obj into StockQuote type
+    """
+    def to_stock_quote(self, symbol: str, df: pd.DataFrame) -> StockQuote:
+        latest = df.iloc[-1]
+
+        return StockQuote(
+            symbol=symbol,
+            date=latest.name.to_pydatetime(),
+            open=float(latest["Open"]),
+            high=float(latest["High"]),
+            low=float(latest["Low"]),
+            close=float(latest["Close"]),
+            volume=int(latest["Volume"]),
+            dividends=float(latest["Dividends"]),
+            stock_splits=float(latest["Stock Splits"]),
+        )
